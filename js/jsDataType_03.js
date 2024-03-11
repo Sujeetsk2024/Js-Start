f505c3ac8743cd5df17e7e0058a404cf04233367
Sujeetsk2024 <162977184+Sujeetsk2024@users.noreply.github.com>
2024-03-11 13:03:27 +0000

    jsDataType 03

---
+++ js/jsDataType_03.js
@@ -0,0 +1,25 @@
+"use strict";  //use fo rtreat all js code as a newer version , now a days all code already in newer version.
+
+//alert("hello")  // in node js alert is not pop up give alert not defined, we are using nodejs not browser.
+
+console.log("Sujeet"); console.log(3+3)   // code should in readable form.
+
+//DATATYPE.
+let name = ("Sujeet");
+let age = 18;
+let isLoggedIn = true;
+
+console.log(typeof age);
+
+//number
+//bigInt
+//string
+//boolean  --> true or false
+//null---> standalone value
+//undefined
+//symbol  --> uniqueness
+
+//object
+
+console.log(typeof null)    //object type
+console.log(typeof undefined) //  undefined type
