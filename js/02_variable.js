ffabd347faff6e13028fa1ac23321aab09693642
Sujeetsk2024 <162977184+Sujeetsk2024@users.noreply.github.com>
2024-03-11 10:58:08 +0000

    js variable

---
+++ js/02_variable.js
@@ -0,0 +1,20 @@
+const acId = 31232
+let acEmail= "Sujeet@gmail.com"
+var acPass = "541230"
+acCity = "Lucknow"  // not a proper code 
+let acState;
+
+console.table([acId,acEmail,acPass,acCity,acState])
+
+//acId = 4534   //not allowed in const
+
+console.log(acId);
+
+acEmail = "Sujeet@google.in"
+acPass = "31545"
+acCity = " Jaipur"
+
+console.table([acId,acEmail,acPass,acCity])
+
+
+/* var not used becoz issue in block and functional scope */
